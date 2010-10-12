0031d38e2dd9a5f063f4bfbc4060dd1e44d20d3b
Tobias Schmid <schmid.tobias@students.unibe.ch>
2010-10-12 22:52:02 +0200

    embedded login into page, added profil page and profile edit (no-content available)

--- qa/app/controllers/UserFuncs.java
+++ qa/app/controllers/UserFuncs.java
@@ -1,23 +1,32 @@
 package controllers;
 
 import models.User;
+import play.mvc.Before;
 import play.mvc.Controller;
 import play.mvc.With;
 
 @With(Secure.class)
 public class UserFuncs extends Controller {
 
+	@Before
+	static void setConnectedUser() {
+		if (Security.isConnected()) {
+			User user = User.get(Security.connected());
+			renderArgs.put("user", user);
+		}
+	}
+
 	public static void showProfile(long id) {
 
-		User user = User.getById(id);
-		render(user);
+		User puser = User.getById(id);
+		render(puser);
 	}
 
 	public static void editProfile(long id) {
 
-		User user = User.getById(id);
+		User puser = User.getById(id);
 
-		render(user);
+		render(puser);
 	}
 
 	public static void saveProfile(long id) {
