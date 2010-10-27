42a148b85d313a89dfa974927dde31b6c8e1c5eb
Tobias Schmid <schmid.tobias@students.unibe.ch>
2010-10-28 00:12:59 +0200

    added jQuery for input placeholder

--- qa/public/javascripts/qa.js
+++ qa/public/javascripts/qa.js
@@ -43,4 +43,21 @@ function displayGraph(data, status) {
     	}
     } );
  }
+ 
+ $(document).ready(function(){ 
+  $('input[type=text]').focus(function(){ 
+    if($(this).val() == $(this).attr('defaultValue'))
+    {
+      $(this).val('');
+    }
+  });
+  
+  $('input[type=text]').blur(function(){
+    if($(this).val() == '')
+    {
+      $(this).val($(this).attr('defaultValue'));
+    } 
+  });
+}); 
+ 
 
