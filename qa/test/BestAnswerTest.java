d1582eecef0a586921e1452a2540725caebb2ac8
Simon Marti <simon.marti@gmx.ch>
2010-10-28 09:23:37 +0200

    added BestAnswerTest own answers can't be selected as best answers

---
+++ qa/test/BestAnswerTest.java
@@ -0,0 +1,53 @@
+import models.Answer;
+import models.Question;
+import models.User;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import play.test.Fixtures;
+import play.test.UnitTest;
+
+public class BestAnswerTest extends UnitTest {
+
+	@Before
+	public void setUp() {
+		Fixtures.deleteAll();
+	}
+
+	@Test
+	public void setBestAnswerSzenario() {
+
+		User jack = new User("Jack", "test@mail.com", "password").save();
+		User bill = new User("Bill", "bill@mail.com", "secret").save();
+		Question question = jack.addQuestion("A title", "My first question");
+		Answer firstAnswer = question.answer(jack, "first answer");
+		Answer secondAnswer = question.answer(bill, "second answer");
+		Answer thirdAnswer = question.answer(bill, "third answer");
+
+		assertFalse(firstAnswer.isBestAnswer());
+		assertFalse(secondAnswer.isBestAnswer());
+		assertFalse(thirdAnswer.isBestAnswer());
+
+		assertTrue(question.canSetBestAnswer());
+
+		assertFalse(firstAnswer.canBeBestAnswer());
+		assertTrue(secondAnswer.canBeBestAnswer());
+		assertTrue(thirdAnswer.canBeBestAnswer());
+
+		question.setBestAnswer(secondAnswer);
+
+		assertFalse(firstAnswer.isBestAnswer());
+		assertTrue(secondAnswer.isBestAnswer());
+		assertFalse(thirdAnswer.isBestAnswer());
+
+		assertTrue(question.canSetBestAnswer());
+
+		question.resetBestAnswer();
+
+		assertFalse(firstAnswer.isBestAnswer());
+		assertFalse(secondAnswer.isBestAnswer());
+		assertFalse(thirdAnswer.isBestAnswer());
+	}
+
+}
