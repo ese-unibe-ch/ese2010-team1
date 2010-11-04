16a1bf1526410ec31fba8bbcb8aab7380ab92d62
Simon Marti <simon.marti@gmx.ch>
2010-11-04 14:56:34 +0100

    added top navigation (dropdown menu and tab bar)

--- qa/public/javascripts/global.js
+++ qa/public/javascripts/global.js
@@ -1,7 +1,14 @@
 $(function() {
 
-	// display scrollbar
+	// display scrollbars
 	$('nav').jScrollPane();
 	$('section').jScrollPane();
+	
+	// pulldown menus
+	$('.pulldown a').click(function() {
+		$(this).next().toggle();
+		$('.pulldown a').not(this).next().hide();
+		return false;
+	});
 	
 });
