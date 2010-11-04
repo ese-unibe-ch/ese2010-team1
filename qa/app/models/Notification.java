91f40865a7bef404e28611a2dc1d3408506fc700
Tobias Schmid <schmid.tobias@students.unibe.ch>
2010-11-04 22:02:54 +0100

    a new entitiy for the notifications

---
+++ qa/app/models/Notification.java
@@ -0,0 +1,34 @@
+package models;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+import play.db.jpa.Model;
+
+@Entity
+public class Notification extends Model {
+
+	@ManyToOne
+	public User owner;
+
+	@ManyToOne
+	public Entry entry;
+
+	public Date timestamp;
+
+	public Boolean isNew;
+
+	public String activity;
+
+	public Notification(User owner, Entry entry, String activity) {
+
+		this.owner = owner;
+		this.entry = entry;
+		this.timestamp = new Date();
+		this.activity = activity;
+		this.isNew = true;
+
+	}
+}
