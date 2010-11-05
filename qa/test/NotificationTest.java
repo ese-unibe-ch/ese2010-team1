3c27ec588da3cf74e69fd5586a4bd5f0b5d2085c
Tobias Schmid <schmid.tobias@students.unibe.ch>
2010-11-05 18:21:35 +0100

    extended test

--- qa/test/NotificationTest.java
+++ qa/test/NotificationTest.java
@@ -1,3 +1,4 @@
+import models.Notification;
 import models.Question;
 import models.User;
 
@@ -32,6 +33,30 @@ public class NotificationTest extends UnitTest {
 
 		assertTrue(user.hasNewNotifications());
 		assertEquals(2, user.getNewNotifications().size());
+
+	}
+
+	@Test
+	public void readNotification() {
+
+		User user = new User("user1", "user1@test.com", "test").save();
+		User user2 = new User("tes", "test@test.com", "test").save();
+
+		Question question = user.addQuestion("test title", "content");
+		question.answer(user2, "mycontent");
+
+		assertTrue(user.hasNewNotifications());
+		assertEquals(1, user.getNewNotifications().size());
+
+		question.addComment(user2, "my comment");
+
+		assertTrue(user.hasNewNotifications());
+		assertEquals(2, user.getNewNotifications().size());
+
+		Notification notification = user.getNewNotifications().get(0);
+		Notification.hasBeenRed(notification.id);
+
+		assertEquals(1, user.getNewNotifications().size());
 
 	}
 }
