8cf9da120d7590fcb14eb08f1a56f78282fdc801
Tobias Schmid <schmid.tobias@students.unibe.ch>
2010-11-15 20:45:42 +0100

    fixed userid bug, created new file for users javascript functions

--- qa/public/javascripts/global.js
+++ qa/public/javascripts/global.js
@@ -27,25 +27,6 @@ $(function() {
 			self.location = $(this).find("a").attr("href");
 	});
 	
-	// profile options
-	$('a.tab').livequery('click', function(event) {
-		var a = this;
-		$.get(profileGet({id: userid, theAction: this.hash.substr(1)}), function(data) {			
-			$('article.profileContent').html(data);
-			$('a.tab').removeClass("active");
-			$(a).addClass("active");
-			
-			if(a.hash.substr(1)=="graph") {
-				loadGraph();
-			}
-		});
-		return false;
-	});
-	
-	
-	$('article.profileContent').load(profileGet({id: userid, theAction: "activities"}));
-
-	
 	// filter questions
 	$('#filter a').click(function() {
 		switch(this.hash) {
@@ -169,31 +150,3 @@ $(function() {
 
 });
 
-
-function loadGraph() {
-	if(!$("#graph")[0].loaded) {
-
-		jQuery.getJSON( graphData({id: userid}), displayGraph);
-	$("#graph")[0].loaded = true;
-	}
-}
-
-function displayGraph(data, status) {
-var points = [];
-
-for(i in data) {
-points.push([data[i].time, data[i].value]);
-}
-
-    $.plot($("#graphcanvas"), [
-        {
-            data: points,
-            lines: { show: true, steps: true },
-            points: { show: false }
-        }
-    ], {
-     xaxis: {
-     mode: "time"
-     }
-    } );
- }
