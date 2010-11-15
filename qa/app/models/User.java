838a24e58ceb8668a9a044fb0ad41e5294e01796
Simon Marti <simon.marti@gmx.ch>
2010-11-15 20:59:14 +0100

    really fixed reputation graph

--- qa/app/models/User.java
+++ qa/app/models/User.java
@@ -199,7 +199,7 @@ public class User extends Model {
 		 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
 		 */
 		public int compare(Point arg0, Point arg1) {
-			return (int) (((Point) arg1).time - ((Point) arg0).time);
+			return (((Point) arg1).time < ((Point) arg0).time) ? 1 : -1;
 		}
 	}
 
