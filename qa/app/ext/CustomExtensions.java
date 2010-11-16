a4cb95d7d0852d7a08f4d715432652cfd595fbcc
Simon Marti <simon.marti@gmx.ch>
2010-11-17 00:41:21 +0100

    nothing

---
+++ qa/app/ext/CustomExtensions.java
@@ -0,0 +1,12 @@
+package ext;
+
+import play.templates.JavaExtensions;
+
+public class CustomExtensions extends JavaExtensions {
+
+	public static String slice(String string, int length) {
+		String output = "";
+		return output;
+	}
+
+}
