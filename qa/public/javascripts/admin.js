afb7359a5e155700ba94bf0b671c034c1181c37b
Tobias Schmid <schmid.tobias@students.unibe.ch>
2010-12-21 21:35:44 +0100

    created commentation for user deactivation

--- qa/public/javascripts/admin.js
+++ qa/public/javascripts/admin.js
@@ -3,13 +3,17 @@ $(function() {
   
   $('a.activateUser').click(function(){
      $('form#deactivationComment').removeClass("hidden");
-     $("section").append("<div id=\"backgroundOverlay\">&nbsp;</div>");
+     $('section').append("<div id=\"backgroundOverlay\">&nbsp;</div>");
+     var action = $(this).attr("href");
+     $('form#deactivationComment').attr("action", action);
      return false;
   });
   
   $('a.cancel').click(function() {
-    $('form#deactivationComment').addClass("hidden"); 
-    $("#backgroundOverlay").detach();
+    $('form#deactivationComment').addClass("hidden").attr("action", "");
+    $('div#backgroundOverlay').detach();
+    $('textarea[name="deactivationReason"]').val("");
+    return false;
   });
   
   
